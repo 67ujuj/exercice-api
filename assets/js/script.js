f03417099eb33a3e4e441d01f94630a26b18fb04
67ujuj <vauclinjulien@gmail.com>
2023-05-25 11:58:33 +0200

    bouton entree et affichage index 0 des titres de films

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 var tag = document.getElementById('tag');
 var bouton = document.getElementById('bouton');
-
+var page = document.getElementById('page');
 // option de l'api tmdb
 const options = {
   method: 'GET',
@@ -10,7 +10,13 @@ const options = {
       'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzNGUzN2Y1YTU4ZTQ3ZGIwMGY4NTkyODU3OWY5MDBmOCIsInN1YiI6IjY0NmUxNjEzMzNhMzc2MDE1OGRjMDRhZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.10TTdpPPusGwjBn81duAdGN3P84qd250flrJJOeCyEs',
   },
 };
-
+// fonction qui permet de clic quand on appuie sur entrée
+function toucheEntree(event) {
+  if (event.keyCode === 13) {
+    document.getElementById('bouton').click();
+  }
+}
+// fin fonction
 bouton.addEventListener('click', function () {
   if (tag.value == '') {
     alert("vous n'avez rien écrit");
@@ -22,6 +28,7 @@ bouton.addEventListener('click', function () {
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
+        page.innerHTML = `${data.results[0].title}`;
         // generer les affiches et infos
         // generer les affiches et infos
         // generer les affiches et infos
